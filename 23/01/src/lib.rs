f7ccd013005c52b5fec806ef2f71e57943dceb80
iot <iot@clt-mob-t-6401>
2024-03-08 12:02:12 +0100

    23/01: start on part 2

--- 23/01/src/lib.rs
+++ 23/01/src/lib.rs
@@ -1,3 +1,7 @@
+// Improvement ideas:
+// - Is the "chars" iterator double ended? If yes, don't put all nums in Vec but
+// simply call next on both front and back until one digit is found each. Don't
+// forget about handling one or no digits.
 pub fn part1(input: &str) -> u32 {
     let mut nums: Vec<char> = Vec::with_capacity(32);
     let mut calibration_sum = 0;
@@ -22,8 +26,16 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    println!("part2");
-    0
+    let marked_input = mark_words_with_digits(input);
+    part1(&marked_input)
+}
+
+fn mark_words_with_digits(input: &str) -> String {
+    let mut marked_string = input.to_owned();
+
+    if marked_string.starts_with("one") {}
+
+    marked_string
 }
 
 #[cfg(test)]
@@ -31,7 +43,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn should_work() {
-        assert_eq!(2 + 3, 5, "works");
+    fn mark_ones() {
+        assert_eq!(mark_words_with_digits("oionejsnonee"), "oi1nejsn1nee");
     }
 }
--- 23/01/src/main.rs
+++ 23/01/src/main.rs
@@ -2,12 +2,13 @@ use aoc_23_01::{part1, part2};
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input/example.txt").expect("Cannot read file.");
+    // let input = fs::read_to_string("input/example.txt").expect("Cannot read file.");
+    let input = fs::read_to_string("input/example2.txt").expect("Cannot read file.");
     // let input = fs::read_to_string("input/input.txt").expect("Cannot read file.");
     print!("{}", input);
 
-    let result_part1 = part1(&input);
-    println!("Result Part 1: {}", result_part1);
+    // let result_part1 = part1(&input);
+    // println!("Result Part 1: {}", result_part1);
 
     let result_part2 = part2(&input);
     println!("Result Part 2: {}", result_part2);
