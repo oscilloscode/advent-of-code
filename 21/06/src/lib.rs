643aeff9920e1fec82c5c1a39f7a795291303f81
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2024-03-17 12:04:44 +0100

    21/06: slow recursive solution
    Every born fish corresponds to a function call

--- 21/06/src/lib.rs
+++ 21/06/src/lib.rs
@@ -1,10 +1,12 @@
 use log::{debug, info, trace};
 
 const BIRTH_COUNT: u8 = 8;
+const RESET_COUNT: u8 = 6;
 
 pub fn part1(implementation: u8, input: &str) -> usize {
     match implementation {
-        0 | 1 => part1_implementation1(input),
+        1 => part1_implementation1(input),
+        0 | 2 => part1_implementation2(input),
         _ => panic!("No implementation {} for part 1", implementation),
     }
 }
@@ -46,7 +48,7 @@ fn part1_implementation1(input: &str) -> usize {
         let mut baby_count = 0;
         for f in &mut fish {
             if *f == 0 {
-                *f = 6;
+                *f = RESET_COUNT;
                 baby_count += 1;
             } else {
                 *f -= 1;
@@ -60,10 +62,66 @@ fn part1_implementation1(input: &str) -> usize {
     fish.len()
 }
 
+// Calls single_fish function which calls itself recursively whenever a new fish is born.
+// Takes approx. 70 - 80 minutes on home desktop, but no memory issues
+fn part1_implementation2(input: &str) -> usize {
+    info!("part1_implementation2");
+
+    let mut line_iter = input.lines();
+    let days = line_iter
+        .next()
+        .expect("Input contains no first line for amount of days")
+        .parse::<usize>()
+        .expect("Can't parse first line for amount of days");
+
+    let mut fish = line_iter
+        .next()
+        .expect("Input contains no first line for amount of days")
+        .trim()
+        .split(',')
+        .map(|x| x.parse::<u8>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut amount = 0;
+
+    for f in fish {
+        amount += single_fish(f, days);
+    }
+    amount
+}
+
+fn single_fish(mut count: u8, mut days: usize) -> usize {
+    debug!("single fish start: count {count} days {days}");
+
+    let mut children = 0;
+    if count > RESET_COUNT {
+        if days >= (count - RESET_COUNT) as usize {
+            days -= (count - RESET_COUNT) as usize;
+            count = RESET_COUNT;
+        } else {
+            return 1;
+        }
+    }
+
+    loop {
+        if days >= ((count + 1) as usize) {
+            days = days - ((count + 1) as usize);
+            children += single_fish(BIRTH_COUNT, days);
+            count = RESET_COUNT;
+        } else {
+            break;
+        }
+    }
+
+    // println!("children: {children}");
+    children + 1
+}
+
 // Just call part1 functions as the difference is only the amount of days.
 pub fn part2(implementation: u8, input: &str) -> usize {
     match implementation {
-        0 | 1 => part1_implementation1(input),
+        1 => part1_implementation1(input),
+        0 | 2 => part1_implementation2(input),
         _ => panic!("No implementation {} for part 2", implementation),
     }
 }
@@ -73,7 +131,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn mark_ones() {
-        assert_eq!(2 + 2, 4);
+    fn single_fish_no_children() {
+        assert_eq!(single_fish(3, 3), 1);
+        assert_eq!(single_fish(7, 3), 1);
+        assert_eq!(single_fish(8, 3), 1);
+        assert_eq!(single_fish(0, 0), 1);
+    }
+
+    #[test]
+    fn single_fish_children() {
+        assert_eq!(single_fish(8, 9), 2);
+        assert_eq!(single_fish(3, 4), 2);
+        assert_eq!(single_fish(3, 10), 2);
+        assert_eq!(single_fish(3, 11), 3);
+        assert_eq!(single_fish(0, 8), 3);
     }
 }
